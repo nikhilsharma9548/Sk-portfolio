19d3a8a6703fd73111f5dd1e1970e60b49f971d0
nikhilsharma9548 <ns630403@gmail.com>
2026-09-23 22:26:23 +0530

    update

--- src/Componenets/Projects.jsx
+++ src/Componenets/Projects.jsx
@@ -11,12 +11,12 @@ const Projects = () => {
         <h1 className='flex items-center justify-center underline text-3xl font-semibold'>Projects</h1>
         <p className="md:text-lg text-base mt-2 text-center pt-5">  Some cool stuff I’ve built recently 👇</p>
 
-        <div className='flex  flex-col gap-5 px-10 md:flex-row  items-center justify-center mt-10'>
-          <div className='bg-gray-800 max-w-xl p-4 rounded-lg shadow-lg'>
+        <div className='flex  flex-col gap-5 md:flex-row  items-center justify-center mt-10'>
+          <div className='bg-gray-800 max-w-xl p-3 rounded-lg shadow-lg'>
             <img src={project1} alt="Project 1" className='w-full h-auto mb-2 rounded' />
             <p className='text-gray-300'>Description of Project 1</p>
           </div>
-          <div className='bg-gray-800 max-w-xl p-4 rounded-lg shadow-lg'>
+          <div className='bg-gray-800 max-w-xl p-3 rounded-lg shadow-lg'>
             <img src={project2} alt="Project 2" className='w-full h-auto mb-2 rounded' />
             <p className='text-gray-300'>Description of Project 2</p>
           </div>
